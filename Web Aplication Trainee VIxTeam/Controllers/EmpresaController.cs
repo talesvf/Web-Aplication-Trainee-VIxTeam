100b05826a422914e4917e30316aedcb3ad6e6a0
talesvf <talesvf11@gmail.com>
2021-11-18 19:34:34 -0300

    Foi criada uma model, view e controller da entidade "Empresa".

---
+++ Web Aplication Trainee VIxTeam/Controllers/EmpresaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_Aplication_Trainee_VIxTeam.Models;
+
+
+namespace Web_Aplication_Trainee_VIxTeam.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web Aplication Trainee VIxTeam/Models/EmpresaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_Aplication_Trainee_VIxTeam.Models
+{
+    public class EmpresaModel
+    {
+        public int CodigoEmpresa { get; set; }
+        public string NomeEmpresa { get; set; }
+        public string NomeFantasiaEmpresa { get; set; }
+        public string CNPJ { get; set; }
+       
+    }
+}
