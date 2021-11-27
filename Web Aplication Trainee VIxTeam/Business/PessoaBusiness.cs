29a59c82dbb0188c78319a6c9740c5cf1b32371d
talesvf <talesvf11@gmail.com>
2021-11-27 20:06:18 -0300

    Criação da classe PessoaBusiness para tratar as regras de negócio e atender aos requisitos 2 solicitados pelo "cliente".

---
+++ Web Aplication Trainee VIxTeam/Business/PessoaBusiness.cs
@@ -0,0 +1,83 @@
+﻿using Web_Aplication_Trainee_VIxTeam.Data;
+using Web_Aplication_Trainee_VIxTeam.Models;
+
+namespace Web_Aplication_Trainee_VIxTeam.Business
+{
+    public static class PessoaBusiness
+    {
+        public static bool VerificaSituacaoPessoaDeletar(PessoaModel pessoaModel)
+        {
+            return pessoaModel.Situacao;
+        }
+        
+        //esta função verifica o estado antes e depois da tentativa de edição para permitir que ela trate casos diferentes.
+        public static int VerificaSituacaoPessoaEditar(PessoaModel pessoaModel, PessoaModel pessoaBD)
+        {
+            //erro 1: a pessoa está 'inativa' e permanece 'inativa'.
+            if (!pessoaModel.Situacao && !pessoaBD.Situacao)
+            {
+                return 1;
+            }
+            //erro 2: a pessoa está 'inativa' e tenta ser 'ativada' enquanto muda outro atributo.(Se nenhum atributo além da situacao for alterado, a edição será salva, 'ativando' a pessoa.
+            if (pessoaModel.Situacao && !pessoaBD.Situacao)
+            {
+                if (pessoaModel.NomePessoa != pessoaBD.NomePessoa || pessoaModel.Email != pessoaBD.Email || pessoaModel.DataNascimento != pessoaBD.DataNascimento || pessoaModel.QtdFilhos != pessoaBD.QtdFilhos || pessoaModel.Salario != pessoaBD.Salario)
+                {
+                    return 2;
+                }
+            }
+            
+            return 0;
+        }
+
+        //como esta classe nao tem acesso ao _context, esta função existe apenas para organização das regras de negócios.
+        public static bool VerificaEmailAoCriar(bool temEmailIgual)
+        {
+            return !temEmailIgual;
+        }
+        //esta função verifica se o email editado é válido.
+        public static bool VerificaEmailAoEditar(PessoaModel pessoaModel, PessoaModel pessoaBD, bool temEmailIgual)
+        {
+            if(pessoaModel.Email == pessoaBD.Email)
+            {
+                return true;
+            }
+            if (temEmailIgual)
+            {
+                return false;
+            }
+            return true;
+        }
+        //esta função verifica se a data de nascimento é superior a 01/01/1990.
+        public static bool VerificaDataNacimento(PessoaModel pessoaModel)
+        {
+            if (pessoaModel.DataNascimento >= new DateTime(1990, 1, 1))
+            {
+                return true;
+            }
+            return false;
+        }
+        //verifica se a quantidade de filhos é maior ou igual a zero.
+        public static bool VerificaQuantidadeDeFilhos(PessoaModel pessoaModel)
+        {
+            if (pessoaModel.QtdFilhos < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        //verifica se o Salário é inferior a 1200 ou superior a 13000.
+        public static int VerificaSalario(PessoaModel pessoaModel)
+        {
+            if(pessoaModel.Salario < 1200)
+            {
+                return -1;
+            }
+            if(pessoaModel.Salario > 13000)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
--- Web Aplication Trainee VIxTeam/Controllers/PessoaModelsController.cs
+++ Web Aplication Trainee VIxTeam/Controllers/PessoaModelsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Web_Aplication_Trainee_VIxTeam.Data;
 using Web_Aplication_Trainee_VIxTeam.Models;
+using Web_Aplication_Trainee_VIxTeam.Business;
 
 namespace Web_Aplication_Trainee_VIxTeam.Controllers
 {
@@ -57,6 +58,31 @@ namespace Web_Aplication_Trainee_VIxTeam.Controllers
         public async Task<IActionResult> Create([Bind("CodigoPessoa,NomePessoa,Email,DataNascimento,QtdFilhos,Salario")] PessoaModel pessoaModel)
         {
             pessoaModel.Situacao = true;
+            if(!PessoaBusiness.VerificaEmailAoCriar(_context.PessoaModel.Any(x => x.Email == pessoaModel.Email)))
+            {
+                ModelState.AddModelError("Regra de Negócio", "O Email inserido já se encontra cadastrado por outra Pessoa.");
+                return View();
+            }
+            if (!PessoaBusiness.VerificaDataNacimento(pessoaModel))
+            {
+                ModelState.AddModelError("Regra de Negócio", "A Data de Nascimento deve ser superior a 01/01/1990.");
+                return View();
+            }
+            if (!PessoaBusiness.VerificaQuantidadeDeFilhos(pessoaModel))
+            {
+                ModelState.AddModelError("Regra de Negócio", "A Quantidade de Filhos deve ser maior ou igual a zero");
+                return View();
+            }
+            if(PessoaBusiness.VerificaSalario(pessoaModel) == -1)
+            {
+                ModelState.AddModelError("Regra de Negócio", "O salário deve ser superior a R$1200,00.");
+                return View();
+            }
+            if (PessoaBusiness.VerificaSalario(pessoaModel) == 1)
+            {
+                ModelState.AddModelError("Regra de Negócio", "O salário deve ser inferior a R$13000,00.");
+                return View();
+            }
             _context.Add(pessoaModel);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -86,16 +112,59 @@ namespace Web_Aplication_Trainee_VIxTeam.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("CodigoPessoa,NomePessoa,Email,DataNascimento,QtdFilhos,Salario,Situacao")] PessoaModel pessoaModel)
         {
+            PessoaModel pessoaBD = _context.PessoaModel.Single(x => x.CodigoPessoa == id);
+
             if (id != pessoaModel.CodigoPessoa)
             {
                 return NotFound();
             }
+            if (PessoaBusiness.VerificaSituacaoPessoaEditar(pessoaModel, pessoaBD) == 1)
+            {
+                ModelState.AddModelError("Regra de Negócio", "Não é possível editar uma Pessoa com a Situação 'Inativa'.");
+                return View();
+            }
+            if (PessoaBusiness.VerificaSituacaoPessoaEditar(pessoaModel, pessoaBD) == 2)
+            {
+                ModelState.AddModelError("Regra de Negócio", "Esta Pessoa possui a situação 'Inativa', mude-a antes de alterar qualuer outra informação.");
+                return View();
+            }
+            if (!PessoaBusiness.VerificaEmailAoEditar(pessoaModel, pessoaBD, _context.PessoaModel.Any(x => x.Email == pessoaModel.Email)))
+            {
+                ModelState.AddModelError("Regra de Negócio", "O Email inserido já se encontra cadastrado por outra pessoa");
+                return View();
+            }
+            if (!PessoaBusiness.VerificaDataNacimento(pessoaModel))
+            {
+                ModelState.AddModelError("Regra de Negócio", "A Data de Nascimento deve ser superior a 01/01/1990.");
+                return View();
+            }
+            if (!PessoaBusiness.VerificaQuantidadeDeFilhos(pessoaModel))
+            {
+                ModelState.AddModelError("Regra de Negócio", "A Quantidade de Filhos deve ser maior ou igual a zero");
+                return View();
+            }
+            if (PessoaBusiness.VerificaSalario(pessoaModel) == -1)
+            {
+                ModelState.AddModelError("Regra de Negócio", "O salário não pode ser inferior a R$1200,00.");
+                return View();
+            }
+            if (PessoaBusiness.VerificaSalario(pessoaModel) == 1)
+            {
+                ModelState.AddModelError("Regra de Negócio", "O salário não pode ser superior a R$13000,00.");
+                return View();
+            }
+            pessoaBD.NomePessoa = pessoaModel.NomePessoa;
+            pessoaBD.Email = pessoaModel.Email;
+            pessoaBD.DataNascimento = pessoaModel.DataNascimento;
+            pessoaBD.QtdFilhos = pessoaModel.QtdFilhos;
+            pessoaBD.Salario = pessoaModel.Salario;
+            pessoaBD.Situacao = pessoaModel.Situacao;
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(pessoaModel);
+                    _context.Update(pessoaBD);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -138,6 +207,12 @@ namespace Web_Aplication_Trainee_VIxTeam.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var pessoaModel = await _context.PessoaModel.FindAsync(id);
+            //Regra de Negócio: Não é possível deletar uma Pessoa com a Situação 'Ativa'.
+            if (PessoaBusiness.VerificaSituacaoPessoaDeletar(pessoaModel))
+            {
+                ModelState.AddModelError("Regra de Negócio", "Não é possível deletar uma Pessoa com a Situação 'Ativa'.");
+                return View(pessoaModel);
+            }
             _context.PessoaModel.Remove(pessoaModel);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
